9a2ec50b587f5e4dccb511c7a445ee8858530b6b
Keisuke Miyako <keisuke.miyako@4D.com>
2026-03-11 21:19:51 +0900

    finish_reason

--- cpp/ct2-server/main.cpp
+++ cpp/ct2-server/main.cpp
@@ -2180,11 +2180,13 @@ int main(int argc, OPTARG_T argv[]) {
                                 tc["function"] = func;
                                 tool_calls.append(tc);
                                 delta["tool_calls"] = tool_calls;
+                                delta["finish_reason"] = "tool_calls";
                             }else{
                                 delta["content"] = token;
+                                choice["finish_reason"] = Json::nullValue;
                             }
                             choice["delta"] = delta;
-                            choice["finish_reason"] = Json::nullValue;
+                            
                             choices.append(choice);
                             root["choices"] = choices;
                             
